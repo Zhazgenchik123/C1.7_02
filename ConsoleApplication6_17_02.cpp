09c4d22ee4101e32cb7ea67ae66b75674178df0f
jaminka <jaminurmatoc04@gmail.com>
2024-02-09 13:43:19 +0545

    Добавьте файлы проекта.

---
+++ ConsoleApplication6_17_02.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+void swap(int *one, int *two) {
+	int temp = *one;
+	*one = *two;
+	*two = temp;
+}
+
+int main(int argc, char** argv)
+{
+	int a = 5, b = 8;
+
+	std::cout << "a = " << a << ", b = " << b << std::endl;
+
+	swap(&a, &b);
+
+	std::cout << "a = " << a << ", b = " << b << std::endl;
+
+	return 0;
+}
